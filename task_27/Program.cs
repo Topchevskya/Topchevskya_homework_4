0c9d0b3ce3ea95470c92aacedb9f22bd485ed63b
Elena <Dafinia@mail.ru>
2022-08-20 23:16:55 +0300

    hw

---
+++ task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+int length = number.Length;
+int[] numbers = new int[length];
+int sum = 0;
+int count = 0;
+while (count < length)
+{
+    numbers[count] = Convert.ToInt32(number[count].ToString());
+    count++;
+}
+for (int i = 0; i < length; i++)
+    sum += numbers[i];
+
+Console.WriteLine(sum);
---
+++ task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+// int[] array = new int[8];
+// var str = string.Join(" ", array);
+// Console.WriteLine(str);
+
+int[] Array = new int[8];
+Random rnumber = new Random();
+for (int i = 0; i < Array.Length; i++)
+{
+Array[i] = rnumber.Next(1,99);
+Console.Write($"{Array[i]} ");
+}
+Console.WriteLine();
